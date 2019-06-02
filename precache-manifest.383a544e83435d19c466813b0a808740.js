e6f9c71702e435a5aff8dca209d1675a3aa4035c
7-Zip <mattkorn129@gmail.com>
2019-06-02 15:49:56 -0400

    Updates

---
+++ precache-manifest.383a544e83435d19c466813b0a808740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3aa33749dfd8b70effcaf9c70114371",
+    "url": "/Lights-Out/index.html"
+  },
+  {
+    "revision": "6878b63ef6488e6f8545",
+    "url": "/Lights-Out/static/css/main.c16dd05b.chunk.css"
+  },
+  {
+    "revision": "dbd9b9d1d09f56a8fe13",
+    "url": "/Lights-Out/static/js/2.63a3d80a.chunk.js"
+  },
+  {
+    "revision": "6878b63ef6488e6f8545",
+    "url": "/Lights-Out/static/js/main.04faf129.chunk.js"
+  },
+  {
+    "revision": "e061cc9e69b74deb68ab",
+    "url": "/Lights-Out/static/js/runtime~main.daba1e25.js"
+  }
+]);
